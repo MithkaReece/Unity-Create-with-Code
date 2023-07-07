305b7d9cd7059a4ac94521d1de054fcff9cd8b32
MithkaReece <reece.waite1@gmail.com>
2023-07-07 22:43:06 +0100

    Challenge 2 Complete
    - Fixed Bugs
    - Added dog fire cooldown

---
+++ CreateWithCode1.4/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float FireCooldown = 0.4f;
+    private System.DateTime LastDogFire;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (LastDogFire.AddSeconds(FireCooldown) < System.DateTime.Now)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                LastDogFire = System.DateTime.Now;
+            }
+        }
+    }
+}
