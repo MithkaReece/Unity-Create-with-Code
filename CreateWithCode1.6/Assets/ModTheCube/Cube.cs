a87a819e748e02c8b488e8138b6acad6e0583d7f
MithkaReece <reece.waite1@gmail.com>
2023-07-08 23:53:15 +0100

    Cube Modifications
    Can live edit:
    -Colour + Opacity (rgba)
    -Rotation Speed
    -Ratio of rotation of axis (x,y,z)
    -Click a randomize button will set all these to random values

---
+++ CreateWithCode1.6/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    public float RotationSpeed = 1;
+    [Range(0f, 1f)]
+    public float RotationX;
+    [Range(0f, 1f)]
+    public float RotationY;
+    [Range(0f, 1f)]
+    public float RotationZ;
+
+    public Color CubeColor;
+
+    public bool Randomize;
+
+    private Vector3 rotation;
+
+    
+    void Update()
+    {
+        if (Randomize)
+        {
+            Randomize = false;
+            RotationX = Random.Range(0f, 1f);
+            RotationY = Random.Range(0f, 1f);
+            RotationZ = Random.Range(0f, 1f);
+            CubeColor = new Color(
+               Random.Range(0f, 1f),  
+               Random.Range(0f, 1f),   
+               Random.Range(0f, 1f),  
+               Random.Range(0f, 1f)  
+            );
+            RotationSpeed = Random.Range(0f, 1000f);
+        }
+        Renderer.material.color = CubeColor;
+        rotation = new Vector3(RotationX, RotationY, RotationZ);
+        transform.Rotate(RotationSpeed * Time.deltaTime * rotation);
+    }
+}
